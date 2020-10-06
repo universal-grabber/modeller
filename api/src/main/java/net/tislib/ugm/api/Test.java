19707885ed9a884eb8bfc7362b0c82921b48b5de
Taleh Ibrahimli <me@talehibrahimli.com>
2020-10-07 01:57:11 +0400

    temp fixes

--- api/src/main/java/net/tislib/ugm/api/Test.java
+++ api/src/main/java/net/tislib/ugm/api/Test.java
@@ -23,7 +23,7 @@ public class Test {
     public static void main(String[] args) throws IOException {
         String pageUrl = "https://www.imdb.com/title/tt0133093";
         Path path = null;
-        String html = FileUtils.readFileToString(new File("/home/taleh/Projects/UniersalDataPlatform/ugm-backend/api/src/main/resources/html/the_matrix_main.html"));
+        String html = FileUtils.readFileToString(new File("/root/universal-grabber-modeller/api/src/main/resources/html/the_matrix_main.html"));
         String modelUrl = "http://ugm.ug.tisserv.net/api/1.0/models?name=imdb/movie&token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0YWxlaCIsImV4cCI6MTkxNzEwOTU1NX0.VQUVPpHuUjQLYgMdit58oXSr1Wp1LeWDXUIpTrA0Ubzl1F3Rz2QHkZJUxVjkNQOlY8DI97wgxRN8nY_KZuaEAw";
 
         ObjectMapper objectMapper = new ObjectMapper();
